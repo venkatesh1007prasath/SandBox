327bb0bfb18b92ba9d93b8a00565f059f781defb
venkatesh1007prasath <102511015+venkatesh1007prasath@users.noreply.github.com>
2022-11-22 13:52:53 +0530

    Create calc.py

---
+++ python_files/calc.py
@@ -0,0 +1,31 @@
+import math
+
+
+def add(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    else:
+        raise TypeError("Integer only accepted")
+
+
+def sub(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return a - b
+    else:
+        raise TypeError("Integer only accepted")
+
+
+def mul(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return a * b
+    else:
+        raise TypeError("Integer only accepted")
+
+
+def div(a, b):
+    if a == 0:
+        raise ValueError("Zero not allowed")
+    elif isinstance(a, int) and isinstance(b, int):
+        return a / b
+    else:
+        raise TypeError("Integer only accepted")
